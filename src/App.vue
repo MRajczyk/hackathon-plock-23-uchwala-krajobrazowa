2e18ea93cb78b85f2d63da3350781d9b72544234
Patryk Klimas <patrykbajos@protonmail.ch>
2023-10-07 16:13:22 +0200

    oddaje polprodukt

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,87 @@
 <script setup>
+import { ref } from "vue";
 import adresy_strefa from "./adresy_strefa";
-console.log(adresy_strefa)
+
+const zones = ['Obszar 1', 'Obszar 2', 'Obszar 3'];
+const carriers = [
+  'Tablice i urządzenia reklamowe',
+  'Szyldy',
+  'Obiekty małej architektury',
+  'Ogrodzenia'
+];
+const locations = ['Na budynkach', 'Na obiektach', 'Wolnostojące'];
+
+const step = ref(1);
+
+const street = ref('');
+const buildingNumber = ref('');
+const failedZoneFetch = ref(false);
+
+const zone = ref(zones[0]);
+const carrier = ref(carriers[0]);
+const location = ref(locations[0]);
+
+function fetchZone() {
+  const result = adresy_strefa.filter(element => {
+    return (element.ULIC_NAZWA === street.value) && (element.NUMER == buildingNumber.value);
+  });
+
+  failedZoneFetch.value = result.length !== 1;
+
+  if(failedZoneFetch.value) {
+    zone.value = zones[0];
+  }
+  else {
+    zone.value = 'Obszar ' + result[0].UK_ID;
+  }
+
+  step.value = 2;
+}
+
 </script>
 
 <template>
   <header>
   </header>
-
   <main>
+    <div>
+      <label>Ulica</label>
+      <input name="street" v-model="street">
+      <br>
+
+      <label>Numer budynku</label>
+      <input name="building_nb" type="number" v-model="buildingNumber">
+      <br>
+
+      <input type="button" value="Znajdź strefę" @click="fetchZone" >
+    </div>
+
+    <div v-if="step > 1">
+      <div>
+        <p v-if="failedZoneFetch">Nie znaleziono strefy. Wybierz strefę manualnie.</p>
+        <label>Strefa</label>
+        <select name="zone" v-model="zone">
+          <option v-for="zone in zones">{{zone}}</option>
+        </select>
+      </div>
+
+      <div>
+        <label>Nośnik</label>
+        <select name="carrier" v-model="carrier">
+          <option v-for="c in carriers">{{c}}</option>
+        </select>
+      </div>
+
+      <div v-if="carrier === 'Szyldy' || carrier.startsWith('Tablice')">
+        <label>Lokalizacja</label>
+        <select name="location" v-model="location">
+          <option v-for="l in locations">{{l}}</option>
+        </select>
+      </div>
+    </div>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
